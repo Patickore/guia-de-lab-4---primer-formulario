2182e7f60f4e96f98f64a97614d1c49938c7a88c
Patrick <patri@LAPTOP-Q3T1BCTA>
2022-10-08 16:58:04 -0600

    Agregar archivos de proyecto.

---
+++ guia de lab 4 - primer formulario/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace guia_de_lab_4___primer_formulario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void btncal_Click(object sender, EventArgs e)
+        {
+            double num = Convert.ToDouble(txtNum1.Text);
+            double num2 = Convert.ToDouble(txtNum2.Text);
+            string ItemSelect = comboOpciones.Text;
+            double resultado;
+
+            switch (ItemSelect)
+
+            {
+                case "Suma":
+
+                    resultado = num + num2;
+                    txtresultado.Text = Convert.ToString(resultado);
+
+                    break;
+                case "Resta":
+
+
+                    resultado = num - num2;
+                    txtresultado.Text = Convert.ToString(resultado);
+                    break;
+
+                case "Multiplicación":
+
+                    resultado = num * num2;
+                    txtresultado.Text = Convert.ToString(resultado);
+                    break;
+                case "División":
+
+                    resultado = num / num2;
+                    txtresultado.Text = Convert.ToString(resultado);
+                    break;
+
+
+
+            }
+        }
+    }
+
+    internal class txtresultado
+    {
+        public static string Text { get; internal set; }
+    }
+
+    internal class txtNum2
+    {
+        public static object Text { get; internal set; }
+    }
+
+    internal class comboOpciones
+    {
+        public static string Text { get; internal set; }
+    }
+
+    internal class txtNum1
+    {
+        internal static object Text;
+    }
+}
+
